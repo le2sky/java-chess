fa440d73e85fa75644c8b519b164e87e46ada294
le2sky <leehaneul990623@gmail.com>
2024-03-30 16:02:51 +0900

    refactor: 출력 로직 개선

--- src/main/java/chess/presentation/view/OutputView.java
+++ src/main/java/chess/presentation/view/OutputView.java
@@ -17,27 +17,27 @@ public class OutputView {
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
-    public void printPieces(Pieces pieces) {
-        CharSequence viewData = converter.convertToViewData(pieces);
-        System.out.printf("%n%s%n", viewData);
-    }
-
     public void printGameEndMessage() {
         System.out.println("체스 게임을 종료합니다.");
     }
 
-    // TODO : StringBuilder로 개선
+    public void printPieces(Pieces pieces) {
+        CharSequence viewData = converter.convertToViewData(pieces);
+        printViewData(viewData);
+    }
+
     public void printScore(Map<Team, Double> scoreBoard) {
-        System.out.printf("%n[점수 현황판]%n");
-        scoreBoard.forEach((team, score) -> System.out.printf("%s 팀 점수 : %s %n", team.name(), score));
-        System.out.println();
+        CharSequence viewData = converter.convertToViewData(scoreBoard);
+        printViewData(viewData);
     }
 
     public void printChessResult(ChessResult chessResult) {
-        System.out.printf("%n[게임 결과]%n");
-        System.out.printf("승자 : %s%n", chessResult.winner().name());
-        System.out.printf("패자 : %s%n", chessResult.loser().name());
-        System.out.println();
+        CharSequence viewData = converter.convertToViewData(chessResult);
+        printViewData(viewData);
+    }
+
+    private void printViewData(CharSequence viewData) {
+        System.out.printf("%n%s%n", viewData);
     }
 
     public void printExceptionMessage(Exception exception) {
--- src/main/java/chess/presentation/view/ViewDataConverter.java
+++ src/main/java/chess/presentation/view/ViewDataConverter.java
@@ -1,7 +1,9 @@
 package chess.presentation.view;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import chess.domain.board.ChessResult;
 import chess.domain.board.Coordinate;
 import chess.domain.board.Pieces;
 import chess.domain.piece.Piece;
@@ -35,5 +37,32 @@ class ViewDataConverter {
         }
 
         return shape.toLowerCase();
+    }
+
+    public CharSequence convertToViewData(Map<Team, Double> scoreBoard) {
+        StringBuilder viewData = new StringBuilder("[점수 현황판]");
+        viewData.append(System.lineSeparator());
+        scoreBoard.forEach((team, score) -> {
+            viewData.append(team.name());
+            viewData.append(" 진영 : ");
+            viewData.append(score);
+            viewData.append("점");
+            viewData.append(System.lineSeparator());
+        });
+
+        return viewData;
+    }
+
+    public CharSequence convertToViewData(ChessResult chessResult) {
+        StringBuilder viewData = new StringBuilder("[게임 결과]");
+        viewData.append(System.lineSeparator());
+        viewData.append("승자 : ");
+        viewData.append(chessResult.winner().name());
+        viewData.append(System.lineSeparator());
+        viewData.append("패자 : ");
+        viewData.append(chessResult.loser().name());
+        viewData.append(System.lineSeparator());
+
+        return viewData;
     }
 }
